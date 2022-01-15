5406470fe13de3d9d29132f4f93e0444e6a38a6e
Kunal Makwana <kunalm039@gmail.com>
2022-01-15 06:36:29 +0000

    schema all added

--- graphtut/server/schema/schema.js
+++ graphtut/server/schema/schema.js
@@ -1,25 +1,41 @@
 const graphql = require('graphql');
-const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema,GraphQLInt} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema,GraphQLInt,GraphQLList} = graphql;
 const _ = require('lodash');
-var books = [
 
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
-];    
+//database init
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
+    ];    
 var authors = [
 
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
     { name: 'Brandon Sanderson', age: 42, id: '2'},
     { name: 'Terry Pratchett', age: 66, id: '3' }
     
-];    
+]; 
+//databse end
+
+
+//object node creation
+//this basically define a node architecture of the graphql
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:()=>({
         id: {type:GraphQLID},
         name:{type:GraphQLString},
-        genre:{type:GraphQLString}
+        genre:{type:GraphQLString},
+        author:{type:AuthorType,
+            resolve(parent,args){
+                console.log(parent);
+                return _.find(authors,{id:parent.authorId});
+            }
+        }
     })
 });
 const AuthorType = new GraphQLObjectType({
@@ -27,9 +43,19 @@ const AuthorType = new GraphQLObjectType({
     fields:()=>({
         id: {type:GraphQLID},
         name:{type:GraphQLString},
-        age:{type:GraphQLInt}
+        age:{type:GraphQLInt},
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                return _.filter(books,{authorId:parent.id});
+            }
+        }
+        
     })
-});
+}); //object node creation end
+
+
+//this is like the root node of a tree whose children are book and author which we previously defined
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
     fields:{
@@ -41,7 +67,30 @@ const RootQuery = new GraphQLObjectType({
                 console.log(typeof(args.id));
                return _.find(books,{id:args.id});
             }
+        },
+        author:{
+            //type defines which object node to use
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                //code to get data from db
+                console.log(typeof(args.id));
+               return _.find(authors,{id:args.id});
+            }
+        },
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                return books
+            }
+        },
+        authors:{
+            type:new GraphQLList(AuthorType),
+            resolve(parent,args){
+                return authors
+            }
         }
+
     }
 });
 module.exports = new GraphQLSchema({
