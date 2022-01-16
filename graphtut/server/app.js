cff26c50f082f83f1be52f21fd04086eef4ddd4e
Kunal Makwana <kunalm039@gmail.com>
2022-01-16 06:20:13 +0000

    client added

--- graphtut/server/app.js
+++ graphtut/server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema.js');
+const mongoose=require('mongoose');
 const app = express();
+const mongourl = 'mongodb+srv://supa:supa@cluster0.fqzv9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongourl);
+mongoose.connection.once('open',()=>{
+    console.log("connected to database")
+})
 app.use('/graphql',graphqlHTTP({
 schema,
 graphiql:true
--- graphtut/server/schema/schema.js
+++ graphtut/server/schema/schema.js
@@ -1,24 +1,26 @@
 const graphql = require('graphql');
-const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema,GraphQLInt,GraphQLList} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema,GraphQLInt,GraphQLList,GraphQLNonNull} = graphql;
 const _ = require('lodash');
+const Book=require('../models/book');
+const Author=require('../models/author');
 
 //database init
-var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+// var books = [
+//     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+//     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+//     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
     
-    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
-    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
-    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
-    ];    
-var authors = [
+//     { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+//     { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+//     { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
+//     ];    
+// var authors = [
 
-    { name: 'Patrick Rothfuss', age: 44, id: '1' },
-    { name: 'Brandon Sanderson', age: 42, id: '2'},
-    { name: 'Terry Pratchett', age: 66, id: '3' }
+//     { name: 'Patrick Rothfuss', age: 44, id: '1' 61e2e2a6af10d16376035553},
+//     { name: 'Brandon Sanderson', age: 42, id: '2' 61e2de01a2ffe03bb6656f9e},
+//     { name: 'Terry Pratchett', age: 66, id: '3' 61e2dd5cee3c149ff7e810b2 }
     
-]; 
+// ]; 
 //databse end
 
 
@@ -32,8 +34,9 @@ const BookType = new GraphQLObjectType({
         genre:{type:GraphQLString},
         author:{type:AuthorType,
             resolve(parent,args){
-                console.log(parent);
-                return _.find(authors,{id:parent.authorId});
+                // console.log(parent);
+                // return _.find(authors,{id:parent.authorId});
+                return Author.findById(parent.authorId)
             }
         }
     })
@@ -47,7 +50,8 @@ const AuthorType = new GraphQLObjectType({
         books:{
             type:new GraphQLList(BookType),
             resolve(parent,args){
-                return _.filter(books,{authorId:parent.id});
+                // return _.filter(books,{authorId:parent.id});
+                return Book.find({authorId:parent.id})
             }
         }
         
@@ -64,8 +68,9 @@ const RootQuery = new GraphQLObjectType({
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
                 //code to get data from db
-                console.log(typeof(args.id));
-               return _.find(books,{id:args.id});
+                // console.log(typeof(args.id));
+            //    return _.find(books,{id:args.id});
+            return Book.findById(args.id);
             }
         },
         author:{
@@ -74,25 +79,70 @@ const RootQuery = new GraphQLObjectType({
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
                 //code to get data from db
-                console.log(typeof(args.id));
-               return _.find(authors,{id:args.id});
+            //     console.log(typeof(args.id));
+            //    return _.find(authors,{id:args.id});
+            return Author.findById(args.id);
+
             }
         },
         books:{
             type:new GraphQLList(BookType),
             resolve(parent,args){
-                return books
+                // return books
+                return Book.find({})
             }
         },
         authors:{
             type:new GraphQLList(AuthorType),
             resolve(parent,args){
-                return authors
+                // return authors
+                return Author.find({})
+
             }
         }
 
     }
 });
+
+const Mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        addAuthor:{
+            type:AuthorType,
+            args: {
+                name:{type:new GraphQLNonNull(GraphQLString)},
+                age:{type:new GraphQLNonNull(GraphQLInt)}
+            },
+            resolve(parent,args){
+                let author=new Author({
+                    name:args.name,
+                    age:args.age
+                });
+              return author.save()  ;
+            }
+        },
+        addBook:{
+            type:BookType,
+            args: {
+                
+                name:{type:new GraphQLNonNull(GraphQLString)},
+                genre:{type:new GraphQLNonNull(GraphQLString)},
+                authorId:{type:new GraphQLNonNull(GraphQLID)}
+            },
+            resolve(parent,args){
+                let book=new Book({
+                    name:args.name,
+                    genre:args.genre,
+                    authorId:args.authorId
+                });
+              return book.save()  ;
+            }
+        }
+    }
+})
+
+
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation:Mutation
 })
